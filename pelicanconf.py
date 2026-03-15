e2f22b1916a3e12f5ae571574ac517016164d0a9
Viktor Ostashevskyi <ostash@ostash.kiev.ua>
2026-03-15 17:21:34 +0100

    Long live 'Below the ABI'

---
+++ pelicanconf.py
@@ -0,0 +1,68 @@
+# Core settings
+DEFAULT_CATEGORY = ""
+DISPLAY_CATEGORIES_ON_MENU = False
+DELETE_OUTPUT_DIRECTORY = True
+# MARKDOWN
+PATH = "content"
+ARTICLE_PATHS = ["posts"]
+SITENAME = "Below the ABI"
+STATIC_PATHS = ["static"]
+TYPOGRIFY = True
+ARTICLE_URL = "posts/{slug}/"
+ARTICLE_SAVE_AS = "posts/{slug}/index.html"
+DRAFT_URL = "drafts/posts/{slug}/"
+DRAFT_ARTICLE_SAVE_AS = "drafts/posts/{slug}/index.html"
+AUTHOR_URL = ""
+AUTHOR_SAVE_AS = ""
+CATEGORY_URL = ""
+CATEGORY_SAVE_AS = ""
+AUTHORS_SAVE_AS = ""
+CATEGORIES_SAVE_AS = ""
+SLUGIFY_SOURCE = "basename"
+TIMEZONE = "Europe/Warsaw"
+LOCALE = "C"
+TEMPLATE_PAGES = {
+    "sitemap.xml": "sitemap.xml",
+    "404.html": "404.html",
+    "robots.txt": "robots.txt",
+}
+DIRECT_TEMPLATES = ["index", "tags", "archives"]  # 'categories', 'authors'
+AUTHOR = "Viktor Ostashevskyi"
+EXTRA_PATH_METADATA = {
+    "static/favicon.ico": {"path": "favicon.ico"},
+    "static/.nojekyll": {"path": ".nojekyll"},
+}
+FEED_ALL_RSS = "feeds/all.rss.xml"
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+CATEGORY_FEED_ATOM = None
+DEFAULT_PAGINATION = 10
+DEFAULT_LANG = "en"
+TRANSLATION_FEED_ATOM = None
+THEME = "theme"
+SITESUBTITLE = "where compilers stop"
+# MENUITEMS [(title, url)]
+# LINKS  [(title, url)]
+SOCIAL = [("GitHub", "https://github.com/ostash")]
+SOCIAL_WIDGET_NAME = None
+
+# Theme specific settings
+DEFAULT_COPYRIGHT_YEAR = 2026
+DEFAULT_LICENSE = "CC-BY-4.0"
+GISCUS = {
+    "repo": "ostash/ostash.github.io",
+    "repo-id": "R_kgDORnAUMA",
+    "category": "Announcements",
+    "category-id": "DIC_kwDORnAUMM4C4bu-",
+    "mapping": "pathname",
+    "strict": "1",
+    "reactions-enabled": "1",
+    "emit-metadata": "0",
+    "input-position": "top",
+    "theme": "dark",
+}
+
+# Things which differs for publish
+SITEURL = ""
+RELATIVE_URLS = True
+ANALYTICS = None
